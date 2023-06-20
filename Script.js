40e753fc7d1acaea9d4337f867190da0acd6eb5b
C4rv4Ih0 <clebersoncarvalhomc5@gmail.com>
2023-06-20 16:46:36 -0300

    :tada: initial commit]

---
+++ Script.js
@@ -0,0 +1,36 @@
+function toggleMode(){
+
+    const html = document.documentElement /*const cria variavel, document. acessa o elemento html */
+    html.classList.toggle('Light')
+
+    // if(html.classList.contains("Light")){ /*ao clicar no botao, se no html estiver modo claro, ele vai remover e vai ficar escuro*/
+
+    //     html.classList.remove('Light')
+    // } 
+    // else{ /*se acionado o botão e o html nao estiver no modo claro ele vai adicionar o modo claro ai html*/
+    //     html.classList.add('Light')
+        
+    // }
+
+    //toda essa lógica pode ser substituida por um seimples comando o toggle//
+
+    // PEGAR A TAG IMG
+
+    const img = document.querySelector("#profile img")
+    
+    //SUBSTITUIR A IMG
+
+    if(html.classList.contains("Light")){
+
+    // SE TIVER MODO CLARO, IMG CLARA
+
+        img.setAttribute('src', '/Rocket site/IMG/BRANCO.png')
+    } 
+    else{ 
+
+    // SE TIVER MODO ESCURO, IMG ESCURA
+
+       img.setAttribute('src', '/Rocket site/IMG/PRETO.png')
+  
+    }
+}
